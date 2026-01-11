02f2a36ec9e8de3e0f92086f66bf30497affc7a2
techcoms <120088252+techcoms@users.noreply.github.com>
2026-01-11 07:58:33 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "project29-ec2-sg"
+  }
+}
+
+resource "aws_instance" "this" {
+  ami           = "ami-03f4878755434977f" # Amazon Linux 2 (ap-south-1)
+  instance_type = "t2.micro"
+  subnet_id     = var.subnet_id
+  security_groups = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "project29-ec2"
+  }
+}
